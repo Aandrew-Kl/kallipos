b163d965ae8b80a8a9311498be8d466a208603dd
Klimedidis Andreas <klhmedidisandreas@gmail.com>
2022-11-14 19:08:04 +0200

    changed complexity format

--- complexity.lua
+++ complexity.lua
@@ -5,7 +5,15 @@ function Image(img)
       f:close()
       local duration = pandoc.utils.stringify(doc.meta.duration) or "Duration has not been set"
       local level = pandoc.utils.stringify(doc.meta.level) or "Level has not been set"
-      local complexity = "Proteinomenos xronos: **" .. duration .. "**, Epipedo diskolias: **" .. level .. "**"
+      local wordcount = pandoc.utils.stringify(doc.meta.words) or "Word count has not been set"
+
+      local complexity = "> Προτεινόμενος χρόνος: **" .. duration .. "**, Επίπεδο δυσκολίας: **" .. level .. "**\n"
+      complexity = complexity .. "> Μέγεθος: **" .. wordcount .. "** λέξεις \n\n" .. " _ _ _ _ _"
       return pandoc.RawInline('markdown',complexity)
     end
 end
+
+
+--    Προτεινόμενος χρόνος     Επίπεδο δυσκολίας     Μέγεθος
+--  ------------------------  --------------------  ----------
+--        
